c90011f01ece91e784ccba02faab11d7508b2fa3
atushikurisu99-cmyk <atushi.kurisu99@gmail.com>
2026-03-31 18:00:39 +0900

    Update StandbyScreen.js

--- StandbyScreen.js
+++ StandbyScreen.js
@@ -37,22 +37,36 @@ window.AppScreens.StandbyScreen = (() => {
 
         <div className="pt-4 flex-1 min-h-0 overflow-hidden">
           <div className="relative h-full overflow-hidden">
-            <div className="absolute inset-x-0 top-3 z-0">
+            <div className="absolute inset-x-0 top-3 z-0 px-2">
               <button
                 type="button"
                 onClick={handleFinishTap}
-                className={`${C.endDutyButtonClass} h-[132px] w-full`}
+                className={`${C.endDutyButtonClass} w-full`}
+                style={{ height: "96px" }}
               >
-                <span className="text-[30px] font-extrabold tracking-[-0.03em]">乗務終了</span>
+                <span className="text-[24px] font-extrabold tracking-[-0.03em]">乗務終了</span>
               </button>
             </div>
+
+            {isStandbySheetOpened ? (
+              <div className="absolute inset-x-0 top-0 z-20 flex justify-center pointer-events-none">
+                <button
+                  type="button"
+                  onClick={toggleStandbySheet}
+                  className="pointer-events-auto h-[28px] min-w-[44px] rounded-full bg-white/92 border border-slate-200 text-[18px] font-black leading-none text-slate-500 shadow-[0_4px_10px_rgba(0,0,0,0.10)]"
+                  aria-label="その他を戻す"
+                >
+                  △
+                </button>
+              </div>
+            ) : null}
 
             <BottomCard
               movable={true}
               standbySheetOffset={standbySheetOffset}
               toggleStandbySheet={toggleStandbySheet}
               beginStandbySheetDrag={beginStandbySheetDrag}
-              isFinishVisible={!!isStandbySheetOpened}
+              isOpened={!!isStandbySheetOpened}
               openOtherSheet={openOtherSheet}
               openHistoryModal={openHistoryModal}
               previewRecords={previewRecords}
