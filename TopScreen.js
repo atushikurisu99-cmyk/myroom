5a426f7ee1b15c3820fa7c624b9263e61b6ae9ab
atushikurisu99-cmyk <atushi.kurisu99@gmail.com>
2026-04-01 01:50:42 +0900

    Update TopScreen.js

--- TopScreen.js
+++ TopScreen.js
@@ -13,20 +13,30 @@ window.AppScreens.TopScreen = (() => {
       openOtherSheet,
       openHistoryModal,
       previewRecords,
-      totalAmount,
+      startupMainStyle,
+      startupOtherStyle,
+      startupLock,
     } = props;
 
     return (
       <div className="flex-1 min-h-0 flex flex-col overflow-hidden">
-        <div className="pt-4 shrink-0" style={{ height: `${C.MAIN_BUTTON_SLOT_HEIGHT}px` }}>
+        <div
+          className="pt-4 shrink-0"
+          style={{
+            height: `${C.MAIN_BUTTON_SLOT_HEIGHT}px`,
+            ...(startupMainStyle || {}),
+          }}
+        >
           <button
             type="button"
             onClick={handleTopMain}
-            disabled={topMainButtonDisabled}
+            disabled={topMainButtonDisabled || startupLock}
             className={`${C.mainButtonBase} ${C.mainButtonShine} ${
               topMainButtonDisabled
-                ? "bg-[linear-gradient(180deg,#d5dbe3,#bcc6d2,#97a3b2)] text-white"
-                : "bg-[linear-gradient(180deg,#5dffcf,#21c79a,#008a6a)] text-white"
+                ? 'bg-[linear-gradient(180deg,#d5dbe3,#bcc6d2,#97a3b2)] text-white'
+                : topMainLabel === '乗務終了'
+                ? 'bg-[linear-gradient(180deg,#ffdf6b,#ffb100,#cc7900)] text-white'
+                : 'bg-[linear-gradient(180deg,#5dffcf,#21c79a,#008a6a)] text-white'
             }`}
           >
             <span className={C.bigButtonText}>{topMainLabel}</span>
@@ -35,14 +45,13 @@ window.AppScreens.TopScreen = (() => {
 
         {renderSharedInfoSpacer()}
 
-        <div ref={topScrollRef} className="flex-1 min-h-0 overflow-hidden">
+        <div ref={topScrollRef} className="flex-1 min-h-0 overflow-y-auto">
           <BottomCard
-            movable={false}
-            standbySheetOffset={0}
+            outerClassName="pt-4 shrink-0"
+            outerStyle={startupOtherStyle || undefined}
             openOtherSheet={openOtherSheet}
             openHistoryModal={openHistoryModal}
             previewRecords={previewRecords}
-            totalAmount={totalAmount}
           />
         </div>
       </div>
