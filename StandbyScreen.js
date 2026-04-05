579292263bdafa59387a8884a11d9a9282184051
atushikurisu99-cmyk <atushi.kurisu99@gmail.com>
2026-04-05 19:38:02 +0900

    Update StandbyScreen.js

--- StandbyScreen.js
+++ StandbyScreen.js
@@ -1,19 +1,20 @@
-window.AppScreens.StandbyScreen=(()=>{
+window.AppScreens.StandbyScreen = (() => {
+  const C = window.AppConstants;
 
-return function StandbyScreen({onStart}){
+  return function StandbyScreen({ handleStartRide }) {
+    return (
+      <div className="flex-1 flex flex-col">
 
-return(
-<div className="flex-1 p-4">
+        <div className="pt-4" style={{ height: `${C.MAIN_BUTTON_SLOT_HEIGHT}px` }}>
+          <button
+            onClick={handleStartRide}
+            className={`${C.mainButtonBase} bg-[linear-gradient(180deg,#5ecbff,#2fa8ff,#0072d9)]`}
+          >
+            <span className={C.bigButtonText}>実車</span>
+          </button>
+        </div>
 
-<button
-onClick={onStart}
-className="w-full h-[140px] bg-blue-500 text-white text-xl rounded-2xl"
->
-実車
-</button>
-
-</div>
-);
-};
-
+      </div>
+    );
+  };
 })();
