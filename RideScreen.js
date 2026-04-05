026e844fd05412002aa884f53955053af647afb2
atushikurisu99-cmyk <atushi.kurisu99@gmail.com>
2026-04-05 19:34:57 +0900

    Update RideScreen.js

--- RideScreen.js
+++ RideScreen.js
@@ -1,4 +1,19 @@
-window.AppScreens = window.AppScreens || {};
-window.AppScreens.RideScreen = () => {
-  return <div className="flex-1" />;
+window.AppScreens.RideScreen=(()=>{
+
+return function RideScreen({onDrop}){
+
+return(
+<div className="flex-1 p-4">
+
+<button
+onClick={onDrop}
+className="w-full h-[140px] bg-yellow-500 text-white text-xl rounded-2xl"
+>
+降車
+</button>
+
+</div>
+);
 };
+
+})();
