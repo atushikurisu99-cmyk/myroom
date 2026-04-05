b7b48bc05b3b3d4c9283594a8a270f4de0c19f98
atushikurisu99-cmyk <atushi.kurisu99@gmail.com>
2026-04-05 19:49:54 +0900

    Update RideScreen.js

--- RideScreen.js
+++ RideScreen.js
@@ -1,19 +1,55 @@
+window.AppScreens = window.AppScreens || {};
 window.AppScreens.RideScreen = (() => {
+  const { BottomNav, RideInfoCard } = window.AppComponents;
   const C = window.AppConstants;
 
-  return function RideScreen({ handleDropOffTap }) {
-    return (
-      <div className="flex-1 flex flex-col">
+  return function RideScreen(props) {
+    const {
+      pickup,
+      rideStartAt,
+      elapsedText,
+      viaStops,
+      handleDropOffTap,
+      goHome,
+      openSimpleHistory,
+      openOtherSheet,
+    } = props;
 
-        <div className="pt-4" style={{ height: `${C.MAIN_BUTTON_SLOT_HEIGHT}px` }}>
+    return (
+      <div className="flex-1 min-h-0 flex flex-col overflow-hidden relative pb-[92px]">
+        <div
+          className="pt-4 shrink-0"
+          style={{ height: `${C.MAIN_BUTTON_SLOT_HEIGHT}px` }}
+        >
           <button
+            type="button"
             onClick={handleDropOffTap}
-            className={`${C.mainButtonBase} bg-[linear-gradient(180deg,#ffe066,#ffb400,#cc7a00)]`}
+            className={`${C.mainButtonBase} ${C.mainButtonShine} bg-[linear-gradient(180deg,#ffe066,#ffb400,#cc7a00)]`}
           >
             <span className={C.bigButtonText}>降車</span>
           </button>
         </div>
 
+        <div
+          className="pt-4 shrink-0"
+          style={{ height: `${C.SHARED_INFO_SLOT_HEIGHT}px` }}
+        >
+          <RideInfoCard
+            pickup={pickup}
+            rideStartAt={rideStartAt}
+            elapsedText={elapsedText}
+            viaStops={viaStops}
+          />
+        </div>
+
+        <div className="flex-1 min-h-0" />
+
+        <BottomNav
+          mode="ride"
+          onHome={goHome}
+          onCenter={openSimpleHistory}
+          onMenu={openOtherSheet}
+        />
       </div>
     );
   };
