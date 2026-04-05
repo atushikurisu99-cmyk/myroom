9eadef561515612f707bc295037e263c20de58bb
atushikurisu99-cmyk <atushi.kurisu99@gmail.com>
2026-04-06 02:39:29 +0900

    Update RideScreen.js

--- RideScreen.js
+++ RideScreen.js
@@ -1,60 +1,20 @@
-window.AppScreens = window.AppScreens || {};
 window.AppScreens.RideScreen = (() => {
-  const { RideInfoCard, BottomNav } = window.AppComponents;
-  const C = window.AppConstants;
+  const { BottomNav } = window.AppComponents;
 
-  return function RideScreen(props) {
-    const {
-      pickup,
-      rideStartAt,
-      elapsedText,
-      viaStops,
-      handleDropOffTap,
-      goHome,
-      openHistoryModal,
-      openOtherSheet,
-    } = props;
-
+  return function RideScreen({ drop }) {
     return (
-      <div className="flex-1 min-h-0 flex flex-col overflow-hidden relative pb-[94px]">
-        <div
-          className="pt-4 shrink-0"
-          style={{ height: `${C.MAIN_BUTTON_SLOT_HEIGHT}px` }}
-        >
+      <div className="h-full relative pb-[90px]">
+
+        <div className="p-4">
           <button
-            type="button"
-            onClick={handleDropOffTap}
-            className={`${C.mainButtonBase} ${C.mainButtonShine}`}
-            style={{
-              background: "linear-gradient(180deg,#ffe066,#ffb400,#cc7a00)",
-            }}
+            onClick={drop}
+            className="w-full h-[120px] bg-orange-400 text-white text-2xl rounded-2xl"
           >
-            <span className={C.bigButtonText}>降車</span>
+            降車
           </button>
         </div>
 
-        <div
-          className="pt-4 shrink-0"
-          style={{ height: `${C.SHARED_INFO_SLOT_HEIGHT}px` }}
-        >
-          <RideInfoCard
-            pickup={pickup}
-            rideStartAt={rideStartAt}
-            elapsedText={elapsedText}
-            viaStops={viaStops}
-          />
-        </div>
-
-        <div className="pt-4 flex-1 min-h-0">
-          <div className="h-full rounded-[28px] bg-[#f7f7f7]" />
-        </div>
-
-        <BottomNav
-          centerLabel="履歴"
-          onHome={goHome}
-          onCenter={openHistoryModal}
-          onMenu={openOtherSheet}
-        />
+        <BottomNav isHome={false} />
       </div>
     );
   };
