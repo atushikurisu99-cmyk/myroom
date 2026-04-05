59bc25c634f2ca58d1f01c347c99946b322b9098
atushikurisu99-cmyk <atushi.kurisu99@gmail.com>
2026-04-06 04:18:30 +0900

    Delete FareScreen.js

--- FareScreen.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function FareScreen(props) {
-  return (
-    <div className="w-full h-full flex items-center justify-center">
-      <div>金額入力画面</div>
-    </div>
-  );
-}
