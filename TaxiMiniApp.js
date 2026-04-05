b2c92fad660fc4e956e9ad16acad4e47f2fbc93d
atushikurisu99-cmyk <atushi.kurisu99@gmail.com>
2026-04-05 19:35:27 +0900

    Update TaxiMiniApp.js

--- TaxiMiniApp.js
+++ TaxiMiniApp.js
@@ -1,32 +1,35 @@
-const { useTaxiAppState } = window.AppHooks;
-const { HeaderCard } = window.AppComponents;
-const TopScreen = window.AppScreens.TopScreen;
+const{useTaxiAppState}=window.AppHooks;
+const TopScreen=window.AppScreens.TopScreen;
+const StandbyScreen=window.AppScreens.StandbyScreen;
+const RideScreen=window.AppScreens.RideScreen;
 
-function TaxiMiniApp() {
-  const { state, actions } = useTaxiAppState();
+function App(){
 
-  return (
-    <div className="w-full h-full bg-gray-200 flex justify-center">
-      <div className="w-full max-w-sm h-full px-4 pt-4 relative">
-        <HeaderCard timeParts={state.timeParts} />
+const{state,actions}=useTaxiAppState();
 
-        {state.screen === "top" && (
-          <TopScreen
-            handleTopMain={actions.handleTopMain}
-            showBottom={state.showBottom}
-            setShowBottom={actions.setShowBottom}
-            handleFinishTap={actions.handleFinishTap}
-          />
-        )}
+return(
+<div className="h-full">
 
-        <div className="fixed bottom-0 left-0 right-0 h-[60px] bg-green-400 flex justify-around items-center">
-          <div>ホーム</div>
-          <div>経費</div>
-          <div>メニュー</div>
-        </div>
-      </div>
-    </div>
-  );
+{state.screen==="top"&&(
+<TopScreen
+mainLabel="乗務開始"
+onMain={actions.handleMain}
+showBottom={state.showBottom}
+setShowBottom={actions.setShowBottom}
+onFinish={actions.handleFinish}
+/>
+)}
+
+{state.screen==="standby"&&(
+<StandbyScreen onStart={actions.handleMain}/>
+)}
+
+{state.screen==="ride"&&(
+<RideScreen onDrop={actions.handleMain}/>
+)}
+
+</div>
+);
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(<TaxiMiniApp />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
