c7ca63b49ba938ec542c4781c83bfff89fffd044
atushikurisu99-cmyk <atushi.kurisu99@gmail.com>
2026-04-05 23:53:24 +0900

    Update StandbyScreen.js

--- StandbyScreen.js
+++ StandbyScreen.js
@@ -1,43 +1,24 @@
-window.AppScreens = window.AppScreens || {};
-window.AppScreens.StandbyScreen = (() => {
-  const { BottomNav } = window.AppComponents;
-  const C = window.AppConstants;
+window.AppScreens.StandbyScreen=(()=>{
 
-  return function StandbyScreen(props) {
-    const {
-      handleStartRide,
-      renderSharedInfoSpacer,
-      goHome,
-      openSimpleHistory,
-      openOtherSheet,
-    } = props;
+const{HeaderArea,MainButton,BottomNav}=window.AppComponents;
 
-    return (
-      <div className="flex-1 min-h-0 flex flex-col overflow-hidden relative pb-[92px]">
-        <div
-          className="pt-4 shrink-0"
-          style={{ height: `${C.MAIN_BUTTON_SLOT_HEIGHT}px` }}
-        >
-          <button
-            type="button"
-            onClick={handleStartRide}
-            className={`${C.mainButtonBase} ${C.mainButtonShine} bg-[linear-gradient(180deg,#5ecbff,#2fa8ff,#0072d9)]`}
-          >
-            <span className={C.bigButtonText}>実車</span>
-          </button>
-        </div>
+return function StandbyScreen({actions}){
 
-        {renderSharedInfoSpacer()}
+return(
+<div style={{height:"100%"}}>
 
-        <div className="flex-1 min-h-0" />
+<HeaderArea>
+<div style={{color:"#fff",fontSize:"40px"}}>12:00</div>
+</HeaderArea>
 
-        <BottomNav
-          mode="ride"
-          onHome={goHome}
-          onCenter={openSimpleHistory}
-          onMenu={openOtherSheet}
-        />
-      </div>
-    );
-  };
+<div style={{padding:"16px"}}>
+<MainButton label="実車" onClick={actions.startRide} color="#0072d9"/>
+</div>
+
+<BottomNav mode="ride" onHome={()=>{}} onCenter={()=>{}} onMenu={()=>{}}/>
+
+</div>
+);
+};
+
 })();
