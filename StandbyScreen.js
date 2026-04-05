a915e2711bbb4f72cf081ab02cafa357c0f216b9
atushikurisu99-cmyk <atushi.kurisu99@gmail.com>
2026-04-05 19:49:31 +0900

    Update StandbyScreen.js

--- StandbyScreen.js
+++ StandbyScreen.js
@@ -1,19 +1,42 @@
+window.AppScreens = window.AppScreens || {};
 window.AppScreens.StandbyScreen = (() => {
+  const { BottomNav } = window.AppComponents;
   const C = window.AppConstants;
 
-  return function StandbyScreen({ handleStartRide }) {
-    return (
-      <div className="flex-1 flex flex-col">
+  return function StandbyScreen(props) {
+    const {
+      handleStartRide,
+      renderSharedInfoSpacer,
+      goHome,
+      openSimpleHistory,
+      openOtherSheet,
+    } = props;
 
-        <div className="pt-4" style={{ height: `${C.MAIN_BUTTON_SLOT_HEIGHT}px` }}>
+    return (
+      <div className="flex-1 min-h-0 flex flex-col overflow-hidden relative pb-[92px]">
+        <div
+          className="pt-4 shrink-0"
+          style={{ height: `${C.MAIN_BUTTON_SLOT_HEIGHT}px` }}
+        >
           <button
+            type="button"
             onClick={handleStartRide}
-            className={`${C.mainButtonBase} bg-[linear-gradient(180deg,#5ecbff,#2fa8ff,#0072d9)]`}
+            className={`${C.mainButtonBase} ${C.mainButtonShine} bg-[linear-gradient(180deg,#5ecbff,#2fa8ff,#0072d9)]`}
           >
             <span className={C.bigButtonText}>実車</span>
           </button>
         </div>
 
+        {renderSharedInfoSpacer()}
+
+        <div className="flex-1 min-h-0" />
+
+        <BottomNav
+          mode="ride"
+          onHome={goHome}
+          onCenter={openSimpleHistory}
+          onMenu={openOtherSheet}
+        />
       </div>
     );
   };
