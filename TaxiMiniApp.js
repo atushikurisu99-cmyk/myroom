2c35918e2d037a1fdd8b3e897adc01e8bfe0b2c8
atushikurisu99-cmyk <atushi.kurisu99@gmail.com>
2026-04-05 23:58:31 +0900

    Update TaxiMiniApp.js

--- TaxiMiniApp.js
+++ TaxiMiniApp.js
@@ -1,22 +1,48 @@
-const{useTaxiAppState}=window.AppHooks;
-const TopScreen=window.AppScreens.TopScreen;
-const StandbyScreen=window.AppScreens.StandbyScreen;
-const RideScreen=window.AppScreens.RideScreen;
+const { useTaxiAppState } = window.AppHooks;
 
-function App(){
+const TopScreen = window.AppScreens.TopScreen;
+const StandbyScreen = window.AppScreens.StandbyScreen;
+const RideScreen = window.AppScreens.RideScreen;
 
-const{state,actions}=useTaxiAppState();
+function App() {
+  const { state, actions } = useTaxiAppState();
 
-if(state.screen==="home"){
-return <TopScreen state={state} actions={actions}/>;
+  const [bottomOpen, setBottomOpen] = React.useState(false);
+
+  const commonProps = {
+    timeParts: state.timeParts,
+    weather: state.weather,
+    totalAmount: state.totalAmount,
+    recordCount: state.recordCount,
+  };
+
+  if (state.screen === "home") {
+    return (
+      <TopScreen
+        {...commonProps}
+        bottomOpen={bottomOpen}
+        setBottomOpen={setBottomOpen}
+        startDuty={actions.startDuty}
+        finishDuty={actions.finishDuty}
+      />
+    );
+  }
+
+  if (!state.isRiding) {
+    return (
+      <StandbyScreen
+        {...commonProps}
+        startRide={actions.startRide}
+      />
+    );
+  }
+
+  return (
+    <RideScreen
+      {...commonProps}
+      drop={actions.drop}
+    />
+  );
 }
 
-if(!state.isRiding){
-return <StandbyScreen actions={actions}/>;
-}
-
-return <RideScreen actions={actions}/>;
-
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
