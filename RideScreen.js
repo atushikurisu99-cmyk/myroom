c215a4f7eac0cfc7eb6edfbc2fe7757eb5fa0411
atushikurisu99-cmyk <atushi.kurisu99@gmail.com>
2026-04-11 16:39:19 +0900

    Update RideScreen.js

--- RideScreen.js
+++ RideScreen.js
@@ -1,22 +1,13 @@
+window.RideScreen = ({ onAdvance }) => {
+  const { FixedTopLayout, RideInfoCard } = window.__AppShared;
 
-function RideScreen({ onNext }) {
   return (
-    <div className="w-full h-screen flex flex-col items-center">
-
-      <div className="w-full bg-green-500 text-white p-6">
-        <div className="text-4xl text-right">05:45</div>
-      </div>
-
-      <button onClick={onNext}
-        className="mt-[-30px] w-[90%] bg-yellow-500 text-white py-6 rounded-2xl text-xl shadow-lg">
-        降車
-      </button>
-
-      <div className="mt-6 bg-white p-4 rounded-xl shadow w-[90%]">
-        取得中...
-      </div>
-
-      <BottomNav />
-    </div>
+    <FixedTopLayout
+      type="ride"
+      buttonLabel="降車"
+      onClick={onAdvance}
+    >
+      <RideInfoCard />
+    </FixedTopLayout>
   );
-}
+};
