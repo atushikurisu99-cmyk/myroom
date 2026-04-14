0b25f70a0bd7a001fbab1d5d92b9776330e08dc7
atushikurisu99-cmyk <atushi.kurisu99@gmail.com>
2026-04-14 13:54:33 +0900

    Update TopScreen.js

--- TopScreen.js
+++ TopScreen.js
@@ -27,17 +27,13 @@ window.AppScreens.TopScreen = (() => {
     return (
       <div className="absolute inset-0 bg-[#dfe5ee] overflow-hidden">
         <div
-          className="absolute inset-x-0 top-0 bg-[#32CD32]"
-          style={{ height: `${L.LINE_5_GREEN_BOTTOM}px` }}
-        />
-
-        <div
           className="absolute"
           style={{
             left: `${L.SIDE}px`,
             right: `${L.SIDE}px`,
             top: `${L.LINE_3_BUTTON_TOP}px`,
             height: `${L.BUTTON_H}px`,
+            zIndex: 6,
             ...(startupMainStyle || {}),
           }}
         >
@@ -64,8 +60,8 @@ window.AppScreens.TopScreen = (() => {
         <div
           className="absolute z-20"
           style={{
-            right: "18px",
-            bottom: "8px",
+            right: "26px",
+            bottom: "118px",
           }}
         >
           <button
@@ -74,7 +70,7 @@ window.AppScreens.TopScreen = (() => {
             className="flex items-center justify-center w-[40px] h-[36px] active:opacity-80"
             aria-label={homeEndSheetOpen ? "終了ボタンを閉じる" : "終了ボタンを開く"}
           >
-            <span className="text-[31px] leading-none font-bold text-slate-300">
+            <span className="text-[28px] font-black text-[#a7adb7] leading-none">
               {homeEndSheetOpen ? "▼" : "▲"}
             </span>
           </button>
@@ -88,6 +84,7 @@ window.AppScreens.TopScreen = (() => {
             bottom: `${C.BOTTOM_NAV_HEIGHT}px`,
             height: `${C.HOME_END_SHEET_HEIGHT}px`,
             pointerEvents: "none",
+            zIndex: 10,
           }}
         >
           <HomeEndDutySheet
