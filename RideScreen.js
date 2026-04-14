2e8d6198f8eb2488ea4577d9edda78171dcb76c5
atushikurisu99-cmyk <atushi.kurisu99@gmail.com>
2026-04-14 13:33:57 +0900

    Update RideScreen.js

--- RideScreen.js
+++ RideScreen.js
@@ -1,16 +1,57 @@
-window.RideScreen = ({ onAdvance }) => {
-  const { FixedTopLayout, RideInfoCard } = window.__AppShared;
+window.AppScreens = window.AppScreens || {};
+window.AppScreens.RideScreen = (() => {
+  const { MainButton, RideInfoCard } = window.AppComponents;
+  const C = window.AppConstants;
+  const L = C.TOP_LAYOUT;
 
-  return (
-    <FixedTopLayout
-      screen="ride"
-      time="05：45"
-      buttonLabel="降車"
-      buttonVariant="ride"
-      onAdvance={onAdvance}
-      bottomNavCenter="履歴"
-      bottomNavActive="center"
-      content={<RideInfoCard />}
-    />
-  );
-};
+  return function RideScreen(props) {
+    const {
+      pickup,
+      rideStartAt,
+      elapsedText,
+      viaStops,
+      handleDropOffTap,
+    } = props;
+
+    return (
+      <div className="absolute inset-0 bg-[#dfe5ee] overflow-hidden">
+        <div
+          className="absolute inset-x-0 top-0 bg-[#32CD32]"
+          style={{ height: `${L.LINE_5_GREEN_BOTTOM}px` }}
+        />
+
+        <div
+          className="absolute"
+          style={{
+            left: `${L.SIDE}px`,
+            right: `${L.SIDE}px`,
+            top: `${L.LINE_3_BUTTON_TOP}px`,
+            height: `${L.BUTTON_H}px`,
+          }}
+        >
+          <MainButton
+            label="降車"
+            type="ride"
+            onClick={handleDropOffTap}
+          />
+        </div>
+
+        <div
+          className="absolute"
+          style={{
+            left: `${L.SIDE}px`,
+            right: `${L.SIDE}px`,
+            top: `${L.LINE_6_CONTENT_TOP}px`,
+          }}
+        >
+          <RideInfoCard
+            pickup={pickup}
+            rideStartAt={rideStartAt}
+            elapsedText={elapsedText}
+            viaStops={viaStops || []}
+          />
+        </div>
+      </div>
+    );
+  };
+})();
